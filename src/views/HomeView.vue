b34ed444506971663a67e61a47a8b327e87cb990
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-22 12:03:05 +0300

    v-if "You didn't add anything to do"

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,30 @@
 <template>
   <div class="home">
-  <h1>To Do App</h1>
-  <div v-for="todo in toDos" :key="todo.id">
-    {{ todo.title }}
-    <p></p>
-  </div>
+    <h1>To Do App</h1>
+    <div v-if="toDos.length">
+      <div v-for="todo in toDos" :key="todo.id">
+        <p>{{ todo.title }}</p>
+      </div>
+    </div>
+    <div v-else>
+      <h3>You didn't add anything to do</h3>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HomeView',
-  data(){
-    return{
-    toDos :[]
+  data() {
+    return {
+      toDos: []
 
     }
   },
-  mounted(){
+  mounted() {
     fetch('http://localhost:3000/ToDos')
-    .then((res)=> res.json()).then((data)=>this.toDos=data)
-    .catch((err)=> console.log(err))
+      .then((res) => res.json()).then((data) => this.toDos = data)
+      .catch((err) => console.log(err))
 
   }
 }
