31bb9dd0037cc4517c4157dc37f9549ebf220961
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-22 16:23:02 +0300

    edit component

---
+++ src/views/todoEdit.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    <h2>EDİT</h2>
+    <form >
+        <label>Title</label>
+        <input type="text" v-model="title" required>
+    
+        <label>Content</label>
+        <input type="text" v-model="content" required>
+    
+        <button @click.prevent="todoEdit">Add</button>
+      </form>
+</div>
+  </template>
+  
+
+<script>
+export default {
+    data(){
+        return{
+            title:'',
+            content:''
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
