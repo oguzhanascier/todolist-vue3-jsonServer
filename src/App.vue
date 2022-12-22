ea527163872cb0718bba7e6927135445987c1db3
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-22 11:23:24 +0300

    app desing

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
 
-  <router-view/>
+  <router-view></router-view>
 </template>
 
 <style>
+#app{
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  text-align: center;
+  color: #f2f2f2;
+  max-width: 850px;
+  margin: 0 auto;
+}
 body{
   background: #663399;
 }
