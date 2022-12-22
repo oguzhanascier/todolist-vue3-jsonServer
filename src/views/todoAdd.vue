bdffbd86ed3166430bb0946250ed5651086c5537
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-22 14:31:48 +0300

    created form section

---
+++ src/views/todoAdd.vue
@@ -0,0 +1,28 @@
+<template>
+  <form @submit.prevent="todoAdd">
+    <label>Title:</label>
+    <input type="text" v-model="title" required>
+
+    <label>Content</label>
+    <input type="text" v-model="content" required>
+  </form>
+</template>
+
+<script>
+export default {
+
+    data(){
+        return{
+            title:'',
+            content:''
+        }
+    },
+    todoAdd(){
+        console.log(this.title, this.content);
+    }
+}
+</script>
+
+<style>
+
+</style>
