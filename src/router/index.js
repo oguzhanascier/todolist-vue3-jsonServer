f99c06dfe5a6895caa12e6efb96e6963337d6cb0
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-22 15:02:41 +0300

    form router

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Form from '../views/todoAdd.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/todo-form',
+    name: 'form',
+    component: Form
   },
 ]
 
