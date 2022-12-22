043fe285c0084f0b2a1af1ef3a631c52411d2a0d
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-22 12:57:01 +0300

    import bootstrap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import BootstrapVue3 from 'bootstrap-vue-3'
 
-createApp(App).use(router).mount('#app')
+import 'bootstrap-icons/font/bootstrap-icons';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+createApp(App).use(BootstrapVue3).use(router).mount('#app')
