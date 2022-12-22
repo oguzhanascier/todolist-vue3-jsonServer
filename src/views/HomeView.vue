7543fc6bbf26541fcceb65be8d85c6db684a2fc2
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-22 12:07:38 +0300

    created todos component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="home">
     <h1>To Do App</h1>
-    <div v-if="toDos.length">
+    <div v-if="toDos.length > 0">
       <div v-for="todo in toDos" :key="todo.id">
         <p>{{ todo.title }}</p>
       </div>
     </div>
     <div v-else>
-      <h3>You didn't add anything to do</h3>
+      <h3>You didn't add anything to do... Refresh the page if you think there is an error</h3>
     </div>
   </div>
 </template>
