ad2b5e3b5ef5fbfd30a1fd21a481fe962a1c97df
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-22 16:41:10 +0300

    call it a day

--- src/views/todoEdit.vue
+++ src/views/todoEdit.vue
@@ -22,9 +22,25 @@ export default {
         return{
             title:'',
             content:'',
-            uri:'http://localhost:3000/ToDos'
+            uri:'http://localhost:3000/ToDos/'+this.id
         }
     },
+    methods:{
+        todoEdit(){
+            fetch(this.uri,{
+                method: 'PATCH',
+                headers:{'Content-Type':'application/json'},
+                body:JSON.stringify({title:this.title, content:this.content})
+            }).then(()=> this.$router.push('/'))
+            .catch((err) => console.log(err))
+        }
+    },
+    mounted(){
+        fetch(this.uri).then((res)=> res.json()).then(data=>{
+            this.title=data.title
+            this.content=data.content
+        } )
+    }
 
 }
 </script>
