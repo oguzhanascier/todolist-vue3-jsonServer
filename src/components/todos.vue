4415457defd85a62ba8493f5c3faf4acce70682d
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-22 12:15:29 +0300

    access props

--- src/components/todos.vue
+++ src/components/todos.vue
@@ -1,9 +1,19 @@
 <template>
-  
+  <div class="todos">
+    <div class="title">
+        <h3>{{todos.title}}</h3>
+    </div>
+    <div class="details">
+        <p class="content">
+            {{ todos.content }}
+        </p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+    props:['todos']
 
 }
 </script>
