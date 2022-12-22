e3ead798d9a8938a0c3070f248c5fd7218718847
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-22 15:51:16 +0300

    created navbar

---
+++ src/components/navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navbar navbar-expand-lg ">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <router-link to="/" tag="a" class="nav-link">HOME</router-link>
+            <router-link to="/todo-form" tag="a" class="nav-link">NEW TODO</router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+nav{
+    background: #663399;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.252);
+    
+}
+.nav-link{
+    color: white;
+}
+
+</style>
