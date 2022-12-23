2478925d0acf5643c21e38eec2c937422aad25a6
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-23 10:25:20 +0300

    get $event from from navbarFilter component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,32 @@
 <template>
-<div>
-  <navbar></navbar>
-  <router-view></router-view>
-</div>
+  <div>
+    <navbar></navbar>
+    <navbar-filter @filterStatus="activeTab = $event"></navbar-filter>
+    <router-view></router-view>
+  </div>
 </template>
 <script>
 import navbar from './components/navbar.vue';
-export default{
-components:{navbar}
+import navbarFilter from './components/navbarFilter.vue';
+export default {
+  components: { navbar, navbarFilter },
+  data() {
+    return {
+
+    }
+  }
 }
 </script>
 <style>
-#app{
+#app {
   font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
   text-align: center;
   color: #f2f2f2;
   max-width: 850px;
   margin: 0 auto;
 }
-body{
+
+body {
   background: #663399 !important;
 }
-
-
-
 </style>
